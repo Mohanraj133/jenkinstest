8836578ebbfc2d70f66645d0469a1f500a75af37
Mohanraj133 <107941522+Mohanraj133@users.noreply.github.com>
2022-07-21 11:05:35 +0000

    addedd new word

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -38,6 +38,10 @@ public class TestGreeter {
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
+     @Test
+  public void greetShouldIncludeGreetingwords() {
+    String someone = "Be Happy......!";
+
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
